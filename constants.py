bacfef285da7478b4b26f0d6365f62ea93edf1eb
Amritha Pallavoor <amrithasp02@gmail.com>
2025-06-05 16:48:52 -0700

    move file paths to constants file

--- constants.py
+++ constants.py
@@ -16,5 +16,13 @@ IMAGERY_DATASETS_FOLDER = Path(
 RAW_FOLDER = Path(DATA_ROOT_FOLDER, "raw")
 GROUND_REFERENCE_FOLDER = Path(RAW_FOLDER, "ground-reference")
 
+# Path to parent remote folder with all missions
+ALL_MISSIONS_REMOTE_FOLDER = "js2s3:ofo-public/drone/missions_01"
+
 # Intermediate
 PHOTOGRAMMETRY_FOLDER = Path(DATA_ROOT_FOLDER, "intermediate", "photogrammetry")
+PREPROCESSING_FOLDER = Path(DATA_ROOT_FOLDER, "intermediate", "preprocessing")
+
+# Inputs for first preprocessing step 1_get_mission_altitude.py
+MISSION_ALTITUDES_FOLDER = Path(PREPROCESSING_FOLDER, "mission_altitudes")
+MISSIONS_OUTSIDE_DTM_LIST = Path("/ofo-share/scratch-amritha/tree-species-scratch/failed_missions3.txt")
