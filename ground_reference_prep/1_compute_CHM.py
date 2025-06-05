c93f19d42b598f6300131868479a8ccc0f9697b0
David Russell <djrussell@ucdavis.edu>
2025-06-05 17:29:43 +0000

    Switch DTM and DSM

--- ground_reference_prep/1_compute_CHM.py
+++ ground_reference_prep/1_compute_CHM.py
@@ -23,45 +23,43 @@ def compute_CHM(
             Path to write the newly-computed CHM to
         resolution (Optional[float], optional):
             Spatial resolution of the CHM in meters. If unset, it will default to the resolution of
-            the DTM. Defaults to None.
+            the DSM. Defaults to None.
     """
     # Read the data
     dtm = rioxarray.open_rasterio(dtm_path, masked=True)
     dsm = rioxarray.open_rasterio(dsm_path, masked=True)
-
     if resolution is None:
         # No resampling should be applied
-        dtm_resampled = dtm
+        dsm_resampled = dsm
     else:
-        if dtm.rio.crs.is_geographic:
+        if dsm.rio.crs.is_geographic:
             raise ValueError(
                 "Reprojection of a geographic CRS to meter units will fail"
             )
         # Determine the current resolution
-        dtm_resolution = dtm.rio.resolution()
-        average_res = np.mean(np.abs(dtm_resolution))
+        average_dsm_resolution = np.mean(np.abs(dsm.rio.resolution()))
 
         # Determine the new pixel width and heights
-        scale_factor = average_res / resolution
-        new_height = int(dtm.rio.height * scale_factor)
-        new_width = int(dtm.rio.width * scale_factor)
+        scale_factor = average_dsm_resolution / resolution
+        new_height = int(dsm.rio.height * scale_factor)
+        new_width = int(dsm.rio.width * scale_factor)
 
         # Use an averaging filter for downsampling and bicubic for upsampling
         resampling = Resampling.average if scale_factor < 1 else Resampling.cubic
         # Perform the resampling
-        dtm_resampled = dtm.rio.reproject(
-            dtm.rio.crs, shape=(new_height, new_width), resampling=resampling
+        dsm_resampled = dsm.rio.reproject(
+            dsm.rio.crs, shape=(new_height, new_width), resampling=resampling
         )
 
-    # Determine if the DSM has a higher resolution than the resampled DTM
+    # Determine if the DTM has a higher resolution than the resampled DSM
     # TODO consider how to make this more robust to data that's in geographic coordinates
-    dsm_higher_resolution = np.mean(np.abs(dsm.rio.resolution())) < np.mean(
-        np.abs(dtm_resampled.rio.resolution())
+    dtm_higher_resolution = np.mean(np.abs(dtm.rio.resolution())) < np.mean(
+        np.abs(dsm_resampled.rio.resolution())
     )
     # If the DSM is higher resolution it will be coarstened, so use averaging. Else use bicubic.
-    resampling = Resampling.average if dsm_higher_resolution else Resampling.cubic
-    # Pixel-align the dsm to the DTM
-    dsm_resampled = dsm.rio.reproject_match(dtm_resampled, resampling=resampling)
+    resampling = Resampling.average if dtm_higher_resolution else Resampling.cubic
+    # Pixel-align the DTM to the DSM
+    dtm_resampled = dtm.rio.reproject_match(dsm_resampled, resampling=resampling)
 
     # Subtract the two products
     chm = dsm_resampled - dtm_resampled
