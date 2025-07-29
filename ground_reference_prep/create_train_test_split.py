da5286ce630338f122756223136ecabd4bd26e90
Amritha Pallavoor <amrithasp02@gmail.com>
2025-07-29 18:42:24 +0000

    split the logic into 2 separate functions

--- ground_reference_prep/create_train_test_split.py
+++ ground_reference_prep/create_train_test_split.py
@@ -22,19 +22,17 @@ MIN_SAMPLES = 3
 TEST_FRACTION = 0.2
 SEED = 35
 
-def hdbscan_spatial_split(
-        plot_gdf, 
-        min_cluster_size=MIN_CLUSTER_SIZE, 
-        max_cluster_size=MAX_CLUSTER_SIZE, 
-        min_samples=MIN_SAMPLES, 
-        test_frac=TEST_FRACTION, 
-        seed=SEED
+def hdbscan_spatial_clusters(
+        plot_gdf,
+        min_cluster_size=MIN_CLUSTER_SIZE,
+        max_cluster_size=MAX_CLUSTER_SIZE,
+        min_samples=MIN_SAMPLES,
     ):
     # Convert to meters based CRS
     plot_gdf = plot_gdf.to_crs(32610)
-
     # Extract centroids of each plot geometry for clustering
     coords = np.column_stack((plot_gdf.geometry.centroid.x, plot_gdf.geometry.centroid.y))
+    
     clusterer = HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples, max_cluster_size=max_cluster_size).fit(coords)
     plot_gdf["cluster"] = clusterer.labels_
 
@@ -46,17 +44,16 @@ def hdbscan_spatial_split(
     # For this, we compute the centroids of each cluster
     # and use a KDTree for efficient nearest neighbor search
     cluster_centroids = valid.groupby("cluster").geometry.apply(lambda g: g.unary_union.centroid)
-    cluster_centroids_coords = np.column_stack((cluster_centroids.x, cluster_centroids.y))
+    cluster_coords = np.column_stack((cluster_centroids.x, cluster_centroids.y))
+    tree = KDTree(cluster_coords)
     cluster_ids = cluster_centroids.index.to_numpy()
-    tree = KDTree(cluster_centroids_coords)
 
     # Compute centroids of noise plot geometries
-    noise_centroids = np.column_stack((noise.geometry.centroid.x, noise.geometry.centroid.y))
+    noise_coords = np.column_stack((noise.geometry.centroid.x, noise.geometry.centroid.y))
     # Query the nearest cluster centroid for each noise point
-    _, nearest_idx = tree.query(noise_centroids)
+    _, nearest_idx = tree.query(noise_coords)
     # Assign the cluster ID of the nearest centroid to the noise points
-    assigned_clusters = cluster_ids[nearest_idx]
-    noise["cluster"] = assigned_clusters
+    noise["cluster"] = cluster_ids[nearest_idx]
 
     # Merge valid and noise plots back together
     plot_gdf = pd.concat([valid, noise], ignore_index=True)
@@ -65,26 +62,30 @@ def hdbscan_spatial_split(
     cluster_hulls = plot_gdf.groupby("cluster").geometry.apply(lambda x: x.unary_union.convex_hull)
     plot_gdf["cluster_convex_hull"] = plot_gdf["cluster"].map(cluster_hulls)
 
+    return plot_gdf
+
+def assign_train_test_clusters(
+        plot_gdf,
+        test_frac=TEST_FRACTION,
+        seed=SEED
+    ):
     # Computer number of plots per cluster
     cluster_sizes = plot_gdf.groupby("cluster").size().reset_index(name="count")
     # frac specifies the fraction of rows to return, 1 means all rows but shuffled
     cluster_sizes = cluster_sizes.sample(frac=1, random_state=seed)
 
-    # Accumulate clusters for the test set until we approximately reach the desired fraction
     test_clusters = []
     cumulative_plots = 0
     total_plots = len(plot_gdf)
+    # Accumulate clusters for the test set until we approximately reach the desired fraction
     for _, row in cluster_sizes.iterrows():
-        cluster_id = row["cluster"]
-        count = row["count"]
         if cumulative_plots / total_plots >= test_frac:
             break
-        test_clusters.append(cluster_id)
-        cumulative_plots += count
-
+        test_clusters.append(row["cluster"])
+        cumulative_plots += row["count"]
+    
     # Assign train/test labels
     plot_gdf["split"] = plot_gdf["cluster"].apply(lambda c: "test" if c in test_clusters else "train")
-
     print(f"Number of clusters (including reassigned noise): {plot_gdf['cluster'].nunique()}")
 
     return plot_gdf
@@ -120,10 +121,11 @@ def main():
     plots_gdf = plots_gdf[plots_gdf['plot_id'].isin(pairs_df['plot_id'])].copy()
 
     # Run spatial HDBSCAN split
-    split_plot_gdf = hdbscan_spatial_split(plots_gdf)
+    clustered_gdf = hdbscan_spatial_clusters(plots_gdf)
+    split_gdf = assign_train_test_clusters(clustered_gdf)
 
     # Merge to include train/test split and cluster ID info in the final results
-    pairs_df = pairs_df.merge(split_plot_gdf[['plot_id', 'split', 'cluster']], on='plot_id', how='left')
+    pairs_df = pairs_df.merge(split_gdf[['plot_id', 'split', 'cluster']], on='plot_id', how='left')
     train_plots = pairs_df[pairs_df['split'] == 'train']['plot_id'].unique()
     test_plots = pairs_df[pairs_df['split'] == 'test']['plot_id'].unique()
     print(f"Train plots: {len(train_plots)}, Test plots: {len(test_plots)}")
@@ -132,7 +134,7 @@ def main():
     print(f"Saved to: {TRAIN_TEST_SPLIT_FILE}")
 
     # Visualize the plots and their train/test split, along with cluster convex hulls
-    visualize_split(split_plot_gdf)
+    visualize_split(split_gdf)
 
 if __name__ == "__main__":
     main()
