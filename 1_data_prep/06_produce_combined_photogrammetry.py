6bee9a3b1995976b39790efd50b91b29fc46efe7
David Russell <djrussell@ucdavis.edu>
2026-01-20 18:17:13 +0000

    Add logic to compute the expected diff between oblique and nadir

--- 1_data_prep/06_produce_combined_photogrammetry.py
+++ 1_data_prep/06_produce_combined_photogrammetry.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+import csv
 
-# path resolved in _bootstrap.py
-from metashape_workflow_functions import make_derived_yaml
 
 import _bootstrap
 from configs.path_config import path_config
+
+# path resolved in _bootstrap.py
+from metashape_workflow_functions import make_derived_yaml
 
 METASHAPE_CONFIG = Path(
     path_config.automate_metashape_path, "config", "config-base.yml"
@@ -15,10 +17,34 @@ def produce_combined_config(imagery_folder: Path):
     # Extract the last part of the path, which is the "<plot_id>_<nadir_id>_<oblique_id>" string
     run_name = imagery_folder.name
 
-    try:
-        _, nadir_id, oblique_id = run_name.split("_")
-    except ValueError:
-        return
+    plot_id, nadir_id, oblique_id = run_name.split("_")
+
+    # Read data with the csv module to avoid additional dependencies
+    with open(path_config.drone_mission_altitudes_per_plot_file, "r") as f:
+        # Skip header
+        derived_altitudes = [l for l in csv.reader(f)][1:]
+
+    # Determine which altitude row corresponds to this imagery set
+    matching_row = [
+        row
+        for row in derived_altitudes
+        if (
+            row[0] == plot_id.lstrip("0")
+            and row[1] == nadir_id.lstrip("0")
+            and row[2] == oblique_id.lstrip("0")
+        )
+    ]
+    if len(matching_row) != 1:
+        raise ValueError(
+            f"Only one row should have been found, instead: {matching_row}"
+        )
+
+    # Extract the average altitudes and compute the difference between them
+    nadir_average_alt = float(matching_row[0][3])
+    oblique_average_alt = float(matching_row[0][4])
+
+    diff = nadir_average_alt - oblique_average_alt
+
     # Find the path to the imagery datasets.
     # Note that we could skip the step of computing oblique and nadir folders and just use one glob
     # specifying that folders are nested three levels deep, but this is a little more robust to
@@ -27,9 +53,9 @@ def produce_combined_config(imagery_folder: Path):
     oblique_dataset_path = Path(imagery_folder, "oblique", oblique_id)
     # Find the sub-folders, corresponding to sub-missions of this dataset, for both oblique and
     # nadir images
-    sub_missions = list(nadir_dataset_path.glob("*")) + list(
-        oblique_dataset_path.glob("*")
-    )
+    nadir_sub_missions = list(nadir_dataset_path.glob("*"))
+    oblique_sub_missions = list(oblique_dataset_path.glob("*"))
+    sub_missions = nadir_sub_missions + oblique_sub_missions
 
     # When we run photogrammetry it's going to be within docker and the data will be mounted in a
     # volume. This will change the paths compared to what's on /ofo-share. This updates the input
@@ -45,16 +71,26 @@ def produce_combined_config(imagery_folder: Path):
         if f.is_dir()
     ]
 
+    # This will tell metashape to shift the files such that the vertical offset between the
+    # average altitude of oblique and nadir images matches the difference in heights above ground
+    # computed from single-mission photogrammetry.
+    paired_offset = {
+        "apply_paired_altitude_offset": True,
+        "paired_altitude_offset": diff,
+        "lower_offset_folders": [str(p) for p in oblique_sub_missions],
+        "upper_offset_folders": [str(p) for p in nadir_sub_missions],
+    }
+
     # Build and override dict that will update the base config with the location of the input images.
     # Also, only generate the DSM-ptcloud orthomosaic. Note, we only need the DTM and mesh-based
     # DSM for downstream experiments, but the ptcloud-based DEM must be computed for building the
     # orthomosaic
     # Finally, the point clouds are removed from the project files to save space.
-
     override_dict = {
         "photo_path": sub_missions,
         "buildOrthomosaic": {"surface": ["DSM-ptcloud"]},
         "buildPointCloud": {"remove_after_export": True},
+        "add_photos": paired_offset,
     }
     # Where to save the config
     output_config_file = Path(
